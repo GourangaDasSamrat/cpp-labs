d1f7faf2d690274100bc10b11937b24fd4d4f2c6
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-13 18:26:41 +0600

    feat(lab-2): add basic C++ program
    - Implement variable declarations
    - Demonstrate constant variable usage
    - Output to console using iostream
    - Recommend clangd extension in VS Code

---
+++ lab-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // declare a integer type variable
+  int score;
+  // assign a value
+  score = 390;
+
+  // declare and assign a string type variable
+  string name = "Gouranga Das Samrat";
+
+  // constant
+  const string myName = "Gouranga Das Samrat";
+
+  // print my name
+  cout << "Gouranga Das Samrat" << endl;
+  return 0;
+}
