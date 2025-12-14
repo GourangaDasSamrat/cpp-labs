c974ec2c656ce563df7f960ec8e3b94ee81085f4
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-14 21:06:11 +0600

    feat(data-types): add C++ primitive data types example

---
+++ lab-3.cpp
@@ -0,0 +1,26 @@
+// primitive data types
+
+// 24:42
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // integer typed data
+  int TeaLeaves = 10;
+
+  // float typed data
+  float waterLevel = 10.23;
+
+  // double typed data
+  double priceOfTea = 100.234;
+
+  // character typed data
+  char TeaGrade = 'A';
+
+  // boolean typed data
+  bool isTeaReady = true;
+
+  cout << isTeaReady << endl;
+  return 0;
+}
