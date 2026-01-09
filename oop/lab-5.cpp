70c5fc5096b5dd414c88b3e1fdd5f2dcc4c027b1
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-09 17:45:14 +0600

    feat(oop): add friend keyword example

---
+++ oop/lab-5.cpp
@@ -0,0 +1,41 @@
+// friend keyword
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Coffee {
+private:
+  string coffeeName;
+  int servings;
+
+public:
+  Coffee(string name, int serve) : coffeeName(name), servings(serve) {}
+
+  friend bool compareServings(const Coffee &coffee1, const Coffee &coffee2);
+
+  void display() const {
+    cout << "Coffee name: " << coffeeName << endl;
+    cout << "Servings: " << servings << "\n" << endl;
+  }
+};
+
+bool compareServings(const Coffee &coffee1, const Coffee &coffee2) {
+  return coffee1.servings > coffee2.servings;
+}
+
+int main() {
+  Coffee cappuccino("Cappuccino", 10);
+  Coffee coldBrew("Cold Brew", 6);
+
+  cappuccino.display();
+  coldBrew.display();
+
+  if (compareServings(cappuccino, coldBrew)) {
+    cout << "Cappuccino is having more servings" << endl;
+  } else {
+    cout << "Cappuccino is having less servings" << endl;
+  }
+  return 0;
+}
