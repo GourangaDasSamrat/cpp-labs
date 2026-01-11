eaab87b00b5e3ca761efba4c2e8b6766bda58327
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-11 17:20:41 +0600

    feat(oop): add example for delegating constructors
    - Implemented `Coffee` class
    - Demonstrated delegating constructor
    - Showed constructor chaining
    - Added basic usage in `main`
    - New C++ OOP lab example

---
+++ oop/lab-7.cpp
@@ -0,0 +1,41 @@
+// Delegating constructor
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Coffee {
+public:
+  string coffeeName;
+  int servings;
+  vector<string> ingredients;
+
+  // Delegating constructor
+  Coffee(string name) : Coffee(name, 1, {"Milk", "Coffee beans", "Ice"}) {}
+
+  Coffee(string name, int serve, vector<string> ingredients)
+      : coffeeName(name), servings(serve), ingredients(ingredients) {}
+
+  void setIngredients(vector<string> item) { ingredients = item; }
+
+  void displayCoffeeDetails() {
+    cout << "Coffee name: " << coffeeName << endl;
+    cout << "Servings: " << servings << endl;
+    cout << "Ingredients: ";
+
+    for (string item : ingredients) {
+      cout << item << ", ";
+    }
+
+    cout << "\n" << endl;
+  }
+};
+
+int main() {
+  Coffee quickCoffee("Quick Coffee");
+  quickCoffee.displayCoffeeDetails();
+
+  return 0;
+}
