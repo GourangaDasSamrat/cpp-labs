1ef326eeca7d69f17908c0795b88d738a5c8ff83
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-13 21:20:16 +0600

    feat(oop): add abstract class and virtual functions
    - Implemented abstract Coffee class
    - Defined virtual methods for coffee prep
    - Created ColdCoffee derived class
    - Created Mocha derived class
    - Demonstrated polymorphism in main

---
+++ oop/lab-9.cpp
@@ -0,0 +1,51 @@
+// Abstract class, virtual function and derived class
+
+#include <iostream>
+
+using namespace std;
+
+// Abstract class
+
+class Coffee {
+public:
+  // virtual functions
+  virtual void prepareIngredients() = 0;
+  virtual void brew() = 0;
+  virtual void serve() = 0;
+
+  void makeCoffee() {
+    prepareIngredients();
+    brew();
+    serve();
+  }
+};
+
+// Derived classes
+
+class ColdCoffee : public Coffee {
+public:
+  void prepareIngredients() override {
+    cout << "Coffee beans, milk and ice is ready." << endl;
+  }
+  void brew() override { cout << "Cold Coffee brewed." << endl; }
+  void serve() override { cout << "Cold Coffee served.\n" << endl; }
+};
+
+class Mocha : public Coffee {
+public:
+  void prepareIngredients() override {
+    cout << "Coffee beans, espresso, steamed milk, and chocolate  is ready."
+         << endl;
+  }
+  void brew() override { cout << "Mocha brewed." << endl; }
+  void serve() override { cout << "Mocha served.\n" << endl; }
+};
+
+int main() {
+  ColdCoffee coldCoffee;
+  Mocha mocha;
+
+  coldCoffee.makeCoffee();
+  mocha.makeCoffee();
+  return 0;
+}
