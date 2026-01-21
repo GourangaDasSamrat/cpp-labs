7a50b08690949c6f8fd58a8ccc2e250aa6ca0e52
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-21 17:45:27 +0600

    feat(stl): add lab-2 for employee data management
    - Define Employee struct and display func
    - Sort employees by salary (descending)
    - Filter high earners using copy_if
    - Calculate total salary with accumulate
    - Demonstrate various STL algorithms

---
+++ stl/lab-2.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Employee {
+  int id;
+  string name;
+  double salary;
+};
+
+void displayEmployee(const Employee& emp) {
+  cout << "ID: " << emp.id << "\n"
+       << "Name: " << emp.name << "\n"
+       << "Salary: $" << emp.salary << "\n"
+       << endl;
+};
+
+int main() {
+  vector<Employee> employees = {
+    {101, "Ada Lovelace", 10000},
+    {102, "Charles Darwin", 20000},
+    {103, "Dmitri Mendeleev", 40000},
+    {104, "Isaac Newton", 30000},
+    {105, "Galileo Galilei", 50000},
+  };
+
+  sort(
+    employees.begin(),
+    employees.end(),
+    [](const Employee& e1, const Employee& e2) {
+      return e1.salary > e2.salary;
+    });
+
+  cout << "Employee sorted by their salary -> highest to lowest: \n" << endl;
+
+  for_each(employees.begin(), employees.end(), displayEmployee);
+
+  vector<Employee> highEarners;
+
+  copy_if(
+    employees.begin(),
+    employees.end(),
+    back_inserter(highEarners),
+    [](const Employee& e) { return e.salary > 30000; });
+
+  cout << "Employees who has salary more than 30000: \n" << endl;
+
+  for_each(highEarners.begin(), highEarners.end(), displayEmployee);
+
+  double totalSum = accumulate(
+    employees.begin(), employees.end(), 0, [](double sum, const Employee& e) {
+      return sum + e.salary;
+    });
+
+  return 0;
+}
